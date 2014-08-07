c6da85bcabf5940c72b71c35899313c56d4685a4
keystore00 <keystore00@gmail.com>
2014-08-07 19:43:54 +0900

    Add qt stylesheet.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "util.h"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
+  a.setStyleSheet(loadEmbeddedOrFile("app_style.css"));
 
   return a.exec();
 }
